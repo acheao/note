112d166b84e5e6c042eaec486e2e3f01eb4684b4
Acheao <273074720linchao@sina.com>
2018-04-15 18:43:43 +0800

    fourth chapter note

--- databases-note/databases_2.sql
+++ databases-note/databases_2.sql
@@ -6,3 +6,30 @@ full outer join 全外连接
 create view abc as select A.ID,A.NAME from A;创建视图abc
 not null约束
 unique(A,b,c)
+check (semester in('Fall','Winter','Spring','Summer'))
+create table course
+(...
+	foreign key(dept_name) references department
+	on delete cascade   	  -- 联级删除
+	on update cascade,		  -- 联级更新
+...);
+deferred 关键字
+
+date '2001-04-25'
+time '09:30:00'
+timestamp '2001-04-25 10:29:01.45'
+current_date,current_time,current_timestamp
+localtime,localtimestamp
+
+dept_name varchar(3,0) default 0
+
+create index studentID_index on student(ID);
+ALTER TABLE `table_name` ADD INDEX index_name ( `column` ) ;
+
+create table A like B;
+
+create table A as 
+(select * 
+from B 
+where a.name = "值") 
+with data;
