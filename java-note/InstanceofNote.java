2f8727ef5f5117af7f8b57cedf9394fd845764a1
Acheao <273074720linchao@sina.com>
2018-04-17 22:53:17 +0800

    java note

---
+++ java-note/InstanceofNote.java
@@ -0,0 +1,11 @@
+
+public class InstanceofNote {
+	public static void main(String[] args){
+		Object obj = "obj";
+		String str = "str";
+		System.out.println("obj是Object类的实例："+(obj instanceof Integer));
+		System.out.println("str是Object类的实例："+(str instanceof Object));		
+		
+	}
+
+}
