9c2ee578ded6b8c610b3f9a57a0d6f80716ea592
acheao <273074720linchao@sina.com>
2019-03-24 21:45:47 +0800

    添加log4j

---
+++ java-note/src/test/log4j/TestLog4j.java
@@ -0,0 +1,10 @@
+package test.log4j;
+import org.apache.log4j.Logger;  
+
+
+public class TestLog4j {
+	public static void main(String[] args) {  
+        Logger logger = Logger.getLogger(TestLog4j.class);  
+        logger.info("zhujiadun");  
+    }  
+}
