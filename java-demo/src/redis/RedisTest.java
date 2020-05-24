37754c2c7c5008ec1013c73c3a525d3ef025a89f
acheao <acheao@foxmail.com>
2020-05-24 15:24:22 +0800

    redis添加密码查询

--- java-demo/src/redis/RedisTest.java
+++ java-demo/src/redis/RedisTest.java
@@ -12,6 +12,7 @@ public class RedisTest {
 	public static void main(String[] args) {
         //连接本地的 Redis 服务
         Jedis jedis = new Jedis("192.168.50.43");
+        jedis.auth("foobared");
         System.out.println("连接成功");
         //查看服务是否运行
         System.out.println("服务正在运行: "+jedis.ping());
