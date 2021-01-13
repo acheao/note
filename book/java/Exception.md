d0d07b275e13c920b5afcf03e88fe81eef191829
acheao <acheao@foxmail.com>
2021-01-13 22:03:02 +0800

    阅读源码

--- book/java/Exception.md
+++ book/java/Exception.md
@@ -1,3 +1,5 @@
 
 - 不合法的参数异常
 throw new IllegalArgumentException("paramater is wrong");
+- 空指针异常
+throw new NullPointerException
--- java-demo/src/StringTest.java
+++ java-demo/src/StringTest.java
@@ -1,3 +1,4 @@
+import java.util.StringJoiner;
 
 public class StringTest {
 	public static void main(String[] args){
@@ -18,5 +19,45 @@ public class StringTest {
         int[] ints = {0x68,0x69,0x1d546,0x70,0x74};
         String str4 = new String(ints,1,3);
         System.out.println(str4);
+        
+        int i1 = str3.indexOf(0x1d546);
+        int i2 = str3.indexOf(0x70);
+        int s1 = str3.indexOf("𝕆");
+        int s2 = str3.indexOf("p");
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(s1);
+        System.out.println(s2);
+        String str5 = "hi𝕆pthi𝕆pt";
+        
+        int i4 = str5.lastIndexOf(0x1d546,2);
+        System.out.println(i4);
+        String str6 = "123456789";
+        String str7 = str6.substring(4,6);
+        System.out.println(str7);
+        String str8 = str5.toUpperCase();
+        System.out.println(str5);
+        System.out.println(str8);
+        String str9 = " fdfhhf fdfdf ";
+        System.out.println(str9.trim());
+        System.out.println(String.join(",","A","B","C","D","E","F"));
+        StringJoiner sj = new StringJoiner(":");
+        sj.add("Ab");
+        sj.add("cd");
+        sj.add("ef");
+        System.out.println(sj.toString());
+        try{
+        	if(true){
+            	throw new NullPointerException();
+            }
+        }catch(Exception e){
+        	e.printStackTrace();
+        }
+        
+        double[] a = new double[10];
+        for(double s : a){
+        	System.out.println(s);
+        }
+        
 	}
 }
